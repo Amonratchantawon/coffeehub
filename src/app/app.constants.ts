a898be1ec90b2b3bd4eecf7396679690fde44c02
amonratCha <amonrat.cha@northbkk.ac.th>
2018-01-23 10:04:22 +0700

    coffeehub

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 export class Constants {
     public static get URL(): string { return "https://eatsyd-test.herokuapp.com"; };
+    // public static get URL(): string { return "https://coffeehub-server.herokuapp.com"; };
 }
